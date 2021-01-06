db12f3f3091d2228653125f3abf4e4673a6480fb
ptmgkira <kira_alves@outlook.com>
2021-01-06 20:06:10 -0300

    GET /Orders

---
+++ backend/src/main/java/com/jpd/japadeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.jpd.japadeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpd.japadeliver.DTO.OrderDTO;
+import com.jpd.japadeliver.entities.Order;
+import com.jpd.japadeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); 
+	}
+}
