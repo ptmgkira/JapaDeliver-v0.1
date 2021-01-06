4ef4f7d9de5accadefb3cf23da0cfe6cdf60daf8
ptmgkira <kira_alves@outlook.com>
2021-01-06 15:16:52 -0300

    Project created

---
+++ backend/src/main/java/com/jpd/japadeliver/JapadeliverApplication.java
@@ -0,0 +1,13 @@
+package com.jpd.japadeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapadeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapadeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jpd/japadeliver/JapadeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpd.japadeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapadeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
