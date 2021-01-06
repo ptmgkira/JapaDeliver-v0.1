0712f31450088c0d14b046d8c7716cd36996f148
ptmgkira <kira_alves@outlook.com>
2021-01-06 20:33:18 -0300

    Post /orders

--- backend/src/main/java/com/jpd/japadeliver/controllers/OrderController.java
+++ backend/src/main/java/com/jpd/japadeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.jpd.japadeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jpd.japadeliver.DTO.OrderDTO;
 import com.jpd.japadeliver.services.OrderService;
@@ -23,4 +27,12 @@ public class OrderController {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
