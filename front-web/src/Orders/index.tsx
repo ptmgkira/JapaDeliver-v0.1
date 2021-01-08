323cb22d65fc9fe1adfd7f7ad3d2f951a9fb6688
ptmgkira <kira_alves@outlook.com>
2021-01-07 22:14:11 -0300

    Front-End Orders complet 50%

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,26 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
 import StepsHeaders from './StepsHeaders';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+   
+    const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+   fetchProducts().then(response => setProducts(response.data)).catch(error => console.log(error))
+  }, [])
+
+
     return (
         <div className="orders-container">
             <StepsHeaders />
+            <ProductsList products={products}/>
         </div>
     )
 }
+
 export default Orders;
