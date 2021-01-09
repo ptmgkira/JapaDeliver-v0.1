dad4675465bd234394845e3946f8041f24837849
Igor Henrique Alves <kira_alves@outlook.com>
2021-01-09 20:12:52 -0300

    Update index.tsx

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as InstagramIcon } from './Instagram.svg';
 function Footer() {
     return (
         <footer className="main-footer">
-            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior <br /> Copyright 2020 Igor Henrique(ptmgkira). All rights reserved.
+            App desenvolvido durante a 2ª no evento Semana DevSuperior <br /> Copyright 2020 Igor Henrique(ptmgkira). All rights reserved.
             <div className="footer-icons">
                 <a href="https://www.instagram.com/?hl=pt-br" target="_new">
                    <InstagramIcon />
